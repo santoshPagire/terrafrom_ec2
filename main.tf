fd2e1dacea8fe1543396c19366578be17ab6a4b4
santosh-eic <santosh.pagire@einfochips.com>
2024-12-30 12:22:39 +0530

    var value change

--- main.tf
+++ main.tf
@@ -2,5 +2,5 @@ resource "aws_instance" "terraform_demo" {
 ami = var.ami
 instance_type = var.instance_type
 
-tags = var.tags
+#tags = var.tags
 }
--- variables.tf
+++ variables.tf
@@ -1,16 +1,14 @@
 variable "ami" {
     type        = string
-    default = "ami-053b12d3152c0cc71"
 }
 
 variable "instance_type" {
     type        = string
-    # default = "t2.micro"
 }
 
-variable "tags" {
-  type = map(string)
-  default = {
-    Name = "test"
-  }
-}
+# variable "tags" {
+#   type = map(string)
+#   default = {
+#     Name = "test"
+#   }
+# }
