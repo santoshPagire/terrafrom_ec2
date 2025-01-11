e4c702ec20738fd5d6e073ac0bd1b7b0780eb18a
santosh-eic <santosh.pagire@einfochips.com>
2025-01-11 19:51:47 +0530

    Revert "backend tf added"
    This reverts commit d3ac9c7a07c635d23d7e571da1046971586703ca.

--- variables.tf
+++ variables.tf
@@ -1,11 +1,9 @@
 variable "ami" {
     type        = string
-    default = "ami-053b12d3152c0cc71"
 }
 
 variable "instance_type" {
     type        = string
-    default = "t2.micro"
 }
 
 # variable "tags" {
